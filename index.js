0c8fa07e92d0a3736ee65ebfd7f87650e1489a73
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-06-28 19:45:19 -0500

    commit tree ed54cedc61894ce82a3e96e948b1e76f6c6f7072

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+//O(n)
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+
+  for(const number of array) {
+    //n steps
+    const compliment = target - number
+    if(compliment in seenNumbers) return true
+    // 1 step
+    seenNumbers[number] = true
+  }
+  return false
 }
 
-/* 
-  Write the Big O time complexity of your function here
+
+
+
+
+// Runtime: O(n^2)
+/*function hasTargetSum(array, target) {
+for(let i = 0; i < array.length; i++) {
+  const compliment = target - array[i]
+  for(let j = i + 1; j < array.length; j++) {
+    if(array[j] === compliment) return true
+  }
+}
+
+return false
+}
 */
 
 /* 
-  Add your pseudocode here
+  Write the Big O time complexity of your function here
+ Runtime: O(n)
+
+
+
+
+
+
+*/
+
+/* 
+  
+  hasTargetSum([1, 2, 3, 4], 6)
+  seenNumbers = {
+    1: true,
+    2: true,
+    3: true
+  }
+
+
+  create an object to keep track of the numbers we've already seen
+  iterate through each number in the array
+    for the current num, identify a compliment that adds to the target (comp = target - num)
+    check if any key on our object is the compliment
+    if so, return true
+    otherwise, add that number to the object
+   
+
+  if I reach the end of the array, return false
 */
 
 /*
